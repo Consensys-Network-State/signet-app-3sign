454b2c553b476c7e48bbf2993666415fdac061ec
Leif DeJong <leifdejong@gmail.com>
2024-12-20 23:41:29 +0100

    fixing ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider as Ds3Provider } from "@ds3/react";
+import { ThemeProvider } from "@ds3/react";
 import { BrowserRouter } from "react-router";
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import Router from './Router';
 import { config } from './wagmi.config'
 import './index.css'
@@ -12,14 +13,16 @@ const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Ds3Provider className="bg-neutral-1 color-neutral-12">
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
-            <Router />
-          </BrowserRouter>
-        </QueryClientProvider>
-      </WagmiProvider>
-    </Ds3Provider>
+    <SafeAreaProvider>
+      <ThemeProvider className="bg-neutral-1 color-neutral-12" config={import.meta.env.DS3}>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+              <Router />
+            </BrowserRouter>
+          </QueryClientProvider>
+        </WagmiProvider>
+      </ThemeProvider>
+    </SafeAreaProvider>
   </StrictMode>,
 )
