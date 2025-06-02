08c3e347cb97e02630c5aaf177b70c4190a7d225
Leif DeJong <leifdejong@gmail.com>
2025-06-02 12:32:57 +0200

    Cleaning up buffer issue and deps

--- vite.config.ts
+++ vite.config.ts
@@ -10,34 +10,12 @@ export default defineConfig({
       buffer: 'buffer/',
     },
   },
-  define: {
-    // Required for Buffer polyfill
-    global: {},
-    'process.env': {},
-  },
   plugins: [
     cui(themeConfig),
     svgr(),
   ],
-  build: { // https://stackoverflow.com/questions/77421447/how-to-solve-require-is-not-defined-in-vite
+  build: {
+    // https://stackoverflow.com/questions/77421447/how-to-solve-require-is-not-defined-in-vite
     commonjsOptions: { transformMixedEsModules: true },
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Create a separate chunk for buffer
-          buffer: ['buffer'],
-        },
-      },
-    },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-    },
-    // Include buffer in optimization
-    include: ['buffer'],
-  }
 })
